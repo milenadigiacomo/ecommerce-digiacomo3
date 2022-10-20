0de710520968313121490b56aae501a6e62c22ff
mile digiacomo <milenadigiacomo@outlook.es>
2022-10-21 01:19:19 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Header() {
+    return (
+        <header id="main-header">
+            <h1>TITULO</h1>
+            <Nav/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+    </nav>
+  );
+};
+
+export default Nav;
